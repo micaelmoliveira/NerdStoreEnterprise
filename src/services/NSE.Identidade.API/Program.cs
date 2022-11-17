586fb48b49f7bb8169fc5f49cfb15bbd970266ea
Micael <micaelmoura.oliveira@gmail.com>
2022-11-17 10:01:32 -0300

    Adicionando suporte jwt

--- src/services/NSE.Identidade.API/Program.cs
+++ src/services/NSE.Identidade.API/Program.cs
@@ -1,13 +1,51 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(beaderOptions =>
+{
+    beaderOptions.RequireHttpsMetadata = true;
+    beaderOptions.SaveToken = true;
+    beaderOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey= true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer= true,
+        ValidateAudience= true,
+        ValidAudience = appSettings.ValidoEm,
+        ValidIssuer= appSettings.Emissor,
+    };
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title= "NerdStore Enterprise Identity API",
+        Description= "Esta é uma API para estudo",
+        Contact = new OpenApiContact() { Name = "Micael Oliveira", Email = "micaelm.oliveira@outlook.com"},
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+    });
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDefaultIdentity<IdentityUser>()
@@ -19,8 +57,13 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();

e64bcc70d4422f686f3acfce745607c8018ce828
Micael <micaelmoura.oliveira@gmail.com>
2022-11-18 14:35:56 -0300

    View catalogo e configuração

---
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+using System.Diagnostics;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : MainController
+    {
+        private readonly ICatalogoService _catalogoService;
+
+        public CatalogoController(ICatalogoService catalogoService)
+        {
+            _catalogoService = catalogoService;
+        }
+
+
+
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _catalogoService.ObterTodos());
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View(await _catalogoService.ObterPorId(id));
+        }
+    }
+}
